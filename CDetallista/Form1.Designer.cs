4a30f4c37c0ead725bfa157f3180ef4421495909
Isaac <rios.isaac87@gmail.com>
2016-03-03 19:03:05 -0600

    code1

--- CDetallista/Form1.Designer.cs
+++ CDetallista/Form1.Designer.cs
@@ -28,25 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(11, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.Size = new System.Drawing.Size(46, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "RFC";
+            this.label1.Text = "RFC:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(53, 6);
+            this.textBox1.Location = new System.Drawing.Point(59, 14);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(140, 20);
             this.textBox1.TabIndex = 1;
@@ -54,42 +58,52 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(203, 33);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Location = new System.Drawing.Point(209, 41);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.Size = new System.Drawing.Size(57, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "ruta";
+            this.label2.Text = "Ruta TXT:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(199, 4);
+            this.button1.Location = new System.Drawing.Point(205, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
             this.button1.Text = "Archivo TXT";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(283, 4);
+            this.button2.ForeColor = System.Drawing.Color.Maroon;
+            this.button2.Location = new System.Drawing.Point(289, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(97, 23);
             this.button2.TabIndex = 4;
             this.button2.Text = "Generar CFDI";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(413, 60);
+            this.ClientSize = new System.Drawing.Size(411, 60);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Complemento Detallista";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,6 +116,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- CDetallista/Form1.cs
+++ CDetallista/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace CDetallista
 {
@@ -14,6 +15,59 @@ namespace CDetallista
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if ((openFileDialog1.ShowDialog() == DialogResult.OK) && (openFileDialog1.FileName.Length > 0))
+                txtfile.Text = openFileDialog1.FileName;
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string txtuser = "PERI8710304I7";
+            string txtpasword = "123456";
+            string xuser = txtuser.Text.Trim(), xpas = txtpasword.Text.Trim(), xcad = "", xerror = "";
+            //string xuser = "XAXX010101000", xpas = "YPfz&vCC#", xcad = "", xerror = "";
+            string namefile = GetNameFile();
+            string fileqr = @"C:\FACINTE\qr_" + namefile + ".png";
+            string filepdf = @"C:\FACINTE\Pdf_" + namefile + ".pdf";
+            string filexml = @"C:\FACINTE\Xml_" + namefile + ".xml";
+            byte[] xpdf = null, xqr = null, xmlx = null;
+            //bool prodcutivo = checprod.Checked;
+
+
+            var cadena = new StreamReader(txtfile.Text.Trim(), System.Text.Encoding.Default);
+            xcad = cadena.ReadToEnd();
+            cadena.Close();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private string GetNameFile()
+        {
+            DateTime fecha = DateTime.Now;
+            int ano = fecha.Year;
+            int mes = fecha.Month;
+            int dia = fecha.Day;
+            int hr = fecha.Hour;
+            int min = fecha.Minute;
+            int seg = fecha.Second;
+
+            string namefile = ano.ToString() + "_" + mes.ToString() + "_" + dia.ToString() + "_" + hr + "_" + min;
+
+            return namefile;
         }
     }
 }
